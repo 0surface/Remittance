5f35de3d976a362adb6db741e2a70e59a6f08430
0surface <tms.0xvoid@gmail.com>
2020-11-06 09:26:14 +0000

    configure compiler, add dependencies

--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,19 @@
 import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import remittanceArtifact from "../../build/contracts/Remittance.json";
 
 const App = {
   web3: null,
   account: null,
   meta: null,
 
-  start: async function() {
+  start: async function () {
     const { web3 } = this;
 
     try {
       // get contract instance
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
-        deployedNetwork.address,
-      );
+      const deployedNetwork = remittanceArtifact.networks[networkId];
+      this.meta = new web3.eth.Contract(remittanceArtifact.abi, deployedNetwork.address);
 
       // get accounts
       const accounts = await web3.eth.getAccounts();
@@ -28,7 +25,7 @@ const App = {
     }
   },
 
-  refreshBalance: async function() {
+  refreshBalance: async function () {
     const { getBalance } = this.meta.methods;
     const balance = await getBalance(this.account).call();
 
@@ -36,7 +33,7 @@ const App = {
     balanceElement.innerHTML = balance;
   },
 
-  sendCoin: async function() {
+  sendCoin: async function () {
     const amount = parseInt(document.getElementById("amount").value);
     const receiver = document.getElementById("receiver").value;
 
@@ -49,7 +46,7 @@ const App = {
     this.refreshBalance();
   },
 
-  setStatus: function(message) {
+  setStatus: function (message) {
     const status = document.getElementById("status");
     status.innerHTML = message;
   },
@@ -57,19 +54,15 @@ const App = {
 
 window.App = App;
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
   } else {
-    console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    );
+    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    );
+    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
   }
 
   App.start();
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const Remittance = artifacts.require("Remittance");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = function (deployer) {
+  deployer.deploy(Remittance);
 };
