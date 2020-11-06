aaedbbd63b49c4a0a9bd004a54efa6a5cf74d074
0surface <tms.0xvoid@gmail.com>
2020-11-06 21:11:24 +0000

    refactor functions into basic working level

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -12,21 +12,23 @@ contract Remittance {
         uint amount;
     }
 
-    mapping(address => remit) ledger;
+    mapping(address => remit) public ledger;
 
     modifier passwordsAreValid(string memory handlerPassword, string memory receiverPassword) {
-        require(!compareStringsbyBytes(handlerPassword, ""), "handlerPassword can not be empty");
-        require(!compareStringsbyBytes(receiverPassword,""), "receiverPassword can not be empty");
-        require(!compareStringsbyBytes(handlerPassword,receiverPassword), "passwords can not be the same");
+        require(keccak256(abi.encodePacked(handlerPassword)) != keccak256(abi.encodePacked("")), "handlerPassword can not be empty");
+        require(keccak256(abi.encodePacked(receiverPassword)) != keccak256(abi.encodePacked("")), "receiverPassword can not be empty");
+        require(keccak256(abi.encodePacked(handlerPassword)) != keccak256(abi.encodePacked(receiverPassword)), "passwords can not be the same");
         _;
     }
 
     constructor(){
-        
+        //TODO: add Ownable library
+        //TODO: add Pausable library
+        //TDDO: add Destructable library
     }
 
-    function hello() public pure returns(uint random) {
-        return 42;
+    function getRemitAmount(address ledgerKey) public view returns(uint amount) {
+        return ledger[ledgerKey].amount;
     }
 
     /*
@@ -41,21 +43,18 @@ contract Remittance {
     {
         return keccak256(abi.encodePacked(handlerPassword, receiverPassword));
     }
-
-    function compareStringsbyBytes(string memory s1, string memory s2) public pure returns(bool){
-        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
-    }
+    
 
     /*
     @dev deposit money into contract ledger
     @param handler = the address of the intermediary which performs ether to other currency exhange
     @param hashedSecret = keccak256 hash
     */
-    function deposit(address handler, bytes32 secretPass) public payable {
-        require(msg.value != 0, "Invalid minimum amount");        
-        remit memory tmp;
-        tmp.secret = secretPass;
-        tmp.amount = msg.value;
+    function deposit(bytes32 secretHash, address handler) public payable {
+        require(msg.value != 0, "Invalid minimum amount");  
+        require(handler != address(0), "Can not deposit into null address");
+        
+        remit memory tmp = remit({secret : secretHash, amount : msg.value});
         ledger[handler] = tmp;
     }
 
@@ -63,12 +62,22 @@ contract Remittance {
     @dev transfer value to caller
     @param 
      */
-    function withdraw(string memory handlerPassword, string memory receiverPassword) passwordsAreValid(handlerPassword,receiverPassword) external {
-        require(generateSecret(handlerPassword, receiverPassword) == ledger[msg.sender].secret, "Incorrect password");
-        
-        (bool success, ) = (msg.sender).call{value: ledger[msg.sender].amount}("");
-        require(success, "withdraw failed");
+    function withdraw(string memory handlerPassword, string memory receiverPassword) 
+        passwordsAreValid(handlerPassword,receiverPassword) external 
+    {        
+        remit memory owed =  ledger[msg.sender];
+        uint _amount = owed.amount;
+        bytes32 _secret = owed.secret;
+
+        require(_amount != 0 && _secret != "", "Sender is not owed a withdrawal");
+        require(generateSecret(handlerPassword, receiverPassword) == _secret, "Incorrect passwords");
+
         ledger[msg.sender].amount = 0;
         ledger[msg.sender].secret = "";
+        
+        //TODO:check msg.sender is contract, "human" - act differently        
+
+        (bool success, ) = (msg.sender).call{value: _amount}("");        
+        require(success, "withdraw failed");        
     }
 }
