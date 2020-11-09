518bc62b188a6a9ec9c46eac39f00eb423a6ca3a
0surface <tms.0xvoid@gmail.com>
2020-11-09 12:38:01 +0000

    add, modify tests

--- test/remittance_deposit_tests.js
+++ test/remittance_deposit_tests.js
@@ -17,45 +17,84 @@ contract("Remittance", (accounts) => {
   const deployer = accounts[0];
   const sender = accounts[1];
   const handler = accounts[2];
-  const nullAddress = "0x0000000000000000000000000000000000000000";
-  const _randomHash = "0x72631ef6a9de404af013211acf2bec80a2d1c9c0b799846fea429a55bf864ee8";
+  const _sent = 20;
+  const _randomHandlerKey = "0x72631ef6a9de404af013211acf2bec80a2d1c9c0b799846fea429a55bf864ee8";
+  const _randomSecretHash = "0xaf01329a52180a2d1c9726311ac4c0b79f2becef6a9de4bf864ee809846fea45";
 
   beforeEach("deploy a fresh contract", async () => {
     remittance = await Remittance.new({ from: deployer });
   });
 
-  it("should revert if deposited towards null address", async () => {
+  it("should revert if given null secret hash", async () => {
+    const nullHash = "0x0000000000000000000000000000000000000000000000000000000000000000";
     await truffleAssert.reverts(
-      remittance.contract.methods.deposit(_randomHash, nullAddress).send({ from: sender, value: 10 }),
-      "Can not deposit into null address"
+      remittance.contract.methods.deposit(nullHash, _randomHandlerKey).send({ from: sender, value: 10 }),
+      "Invalid secretHash value"
     );
   });
 
-  it("should revert if given null hash", async () => {
+  it("should revert if given null handler key", async () => {
     const nullHash = "0x0000000000000000000000000000000000000000000000000000000000000000";
     await truffleAssert.reverts(
-      remittance.contract.methods.deposit(nullHash, nullAddress).send({ from: sender, value: 10 }),
-      "Invalid hash value"
+      remittance.contract.methods.deposit(_randomSecretHash, nullHash).send({ from: sender, value: 10 }),
+      "Invalid handlerKey value"
     );
   });
 
   it("should revert if sent amount is 0", async () => {
     await truffleAssert.reverts(
-      remittance.contract.methods.deposit(_randomHash, handler).send({ from: sender, value: 0 }),
+      remittance.contract.methods.deposit(_randomSecretHash, _randomHandlerKey).send({ from: sender, value: 0 }),
       "Invalid minimum amount"
     );
   });
 
-  it("should record sent amount as owed in storage", async () => {
-    const _sent = 20;
-    const expected = new BN(_sent);
+  it("should revert if input hashes are identical", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.deposit(_randomSecretHash, _randomSecretHash).send({ from: sender, value: _sent }),
+      "secretHash and handlerKey can not be identical"
+    );
+  });
 
-    const remitBefore = await remittance.ledger.call(handler);
-    await remittance.contract.methods.deposit(_randomHash, handler).send({ from: sender, value: _sent });
-    const remitAfter = await remittance.ledger.call(handler);
+  it("should record sent amount as owed in storage", async () => {
+    const expectedAmount = new BN(_sent);
+    const expectedSecret = _randomSecretHash;
+
+    const remitBefore = await remittance.ledger.call(_randomHandlerKey);
+    await remittance.contract.methods.deposit(_randomSecretHash, _randomHandlerKey).send({ from: sender, value: _sent });
+    const remitAfter = await remittance.ledger.call(_randomHandlerKey);
 
     const actual = remitAfter.amount.sub(remitBefore.amount);
 
-    expect(actual).to.be.a.bignumber.that.equals(expected);
+    expect(actual).to.be.a.bignumber.that.equals(expectedAmount);
+    expect(_randomSecretHash).equals(expectedSecret);
+  });
+
+  it("should revert if given active deposit key", async () => {
+    const handlerPassword = "123456";
+    const receiverPassword = "abcdef";
+
+    const expectedHandlerKey = web3.utils.soliditySha3(
+      { type: "address", value: handler },
+      { type: "string", value: handlerPassword }
+    );
+    const expectedHashedSecret = web3.utils.soliditySha3(
+      { type: "bytes32", value: expectedHandlerKey },
+      { type: "string", value: receiverPassword }
+    );
+
+    const secrets = await remittance.contract.methods.generateSecret(handler, handlerPassword, receiverPassword).call();
+    assert.strictEqual(secrets.handlerKey, expectedHandlerKey, "did not generate expected handler key");
+    assert.strictEqual(secrets.hashedSecret, expectedHashedSecret, "did not generate expected hashed secret");
+
+    const depositTxObj = await remittance.contract.methods
+      .deposit(secrets.hashedSecret, secrets.handlerKey)
+      .send({ from: sender, value: _sent });
+
+    assert.isDefined(depositTxObj, "deposit transaction did not get mined/executed");
+
+    await truffleAssert.reverts(
+      remittance.contract.methods.deposit(secrets.hashedSecret, secrets.handlerKey).send({ from: sender, value: _sent }),
+      "Invalid, handler Key has active deposit"
+    );
   });
 });
--- test/remittance_withdraw_tests.js
+++ test/remittance_withdraw_tests.js
@@ -14,45 +14,106 @@ contract("Remittance", (accounts) => {
   });
 
   let remittance;
+  const _sent = 20;
+  const handlerPassword = "123456";
+  const receiverPassword = "abcdef";
   const deployer = accounts[0];
   const sender = accounts[1];
   const handler = accounts[2];
   const randomAddress = accounts[3];
-  const _randomHash = "0x72631ef6a9de404af013211acf2bec80a2d1c9c0b799846fea429a55bf864ee8";
+  const nullHash = "0x0000000000000000000000000000000000000000000000000000000000000000";
 
-  beforeEach("deploy a fresh contract", async () => {
-    remittance = await Remittance.new({ from: deployer });
-  });
-
-  it("should revert when given passwords are incorrect", async () => {
-    const _sent = 20;
-    const handlerPassword = "123456";
-    const receiverPassword = "abcdef";
-
-    const depositTxObj = await remittance.contract.methods.deposit(_randomHash, handler).send({ from: sender, value: _sent });
-
+  it("should NOT double successful withdrawals", async () => {
+    //Arrange
+    const handlerPassword = "123456789";
+    const receiverPassword = "abcdefghi";
+    const expectedHandlerKey = web3.utils.soliditySha3(
+      { type: "address", value: handler },
+      { type: "string", value: handlerPassword }
+    );
+    const expectedHashedSecret = web3.utils.soliditySha3(
+      { type: "bytes32", value: expectedHandlerKey },
+      { type: "string", value: receiverPassword }
+    );
+    const secrets = await remittance.contract.methods.generateSecret(handler, handlerPassword, receiverPassword).call();
+    assert.strictEqual(secrets.handlerKey, expectedHandlerKey, "did not generate expected handler key");
+    assert.strictEqual(secrets.hashedSecret, expectedHashedSecret, "did not generate expected hashed secret");
+    const depositTxObj = await remittance.contract.methods
+      .deposit(secrets.hashedSecret, secrets.handlerKey)
+      .send({ from: sender, value: _sent });
     assert.isDefined(depositTxObj, "deposit function did not get mined/execute");
+    const withdrawTxObj = await remittance.contract.methods
+      .withdraw(handlerPassword, receiverPassword)
+      .send({ from: handler, value: 0 });
+    assert.isDefined(withdrawTxObj, "withdraw function did not get mined/execute");
 
+    //Act, Assert
     await truffleAssert.reverts(
       remittance.contract.methods.withdraw(handlerPassword, receiverPassword).send({ from: handler, value: 0 }),
-      "Incorrect password"
+      "Sender is not owed a withdrawal"
     );
   });
 
-  it("should give owed money when both passwords are correct and inorder", async () => {
-    const _sent = 20;
-    const handlerPassword = "123456";
-    const receiverPassword = "abcdef";
+  it("should clear ledger after successful withdrawal", async () => {
+    //Arrange
+    const handlerPassword = "987654321";
+    const receiverPassword = "ihgfedcba";
+    const expectedHandlerKey = web3.utils.soliditySha3(
+      { type: "address", value: handler },
+      { type: "string", value: handlerPassword }
+    );
+    const expectedHashedSecret = web3.utils.soliditySha3(
+      { type: "bytes32", value: expectedHandlerKey },
+      { type: "string", value: receiverPassword }
+    );
+    const secrets = await remittance.contract.methods.generateSecret(handler, handlerPassword, receiverPassword).call();
+    assert.strictEqual(secrets.handlerKey, expectedHandlerKey, "did not generate expected handler key");
+    assert.strictEqual(secrets.hashedSecret, expectedHashedSecret, "did not generate expected hashed secret");
+    const depositTxObj = await remittance.contract.methods
+      .deposit(secrets.hashedSecret, secrets.handlerKey)
+      .send({ from: sender, value: _sent });
+    assert.isDefined(depositTxObj, "deposit function did not get mined/executed");
 
-    const hashKey = web3.utils.soliditySha3(
-      { type: "string", value: handlerPassword },
+    const remitBeforeWithdrawal = await remittance.ledger.call(secrets.handlerKey);
+    const withdrawTxObj = await remittance.contract.methods
+      .withdraw(handlerPassword, receiverPassword)
+      .send({ from: handler, value: 0 });
+    assert.isDefined(withdrawTxObj, "withdraw function did not get mined/executed");
+
+    //Act
+    const remitAfterWithdrawal = await remittance.ledger.call(secrets.handlerKey);
+
+    //Assert
+    assert.notEqual(remitBeforeWithdrawal.amount, remitAfterWithdrawal.amount, "ledger amount not cleared after withdrawl");
+    assert.strictEqual("0", remitAfterWithdrawal.amount.toString(10), "ledger amount sert to zero after withdrawl");
+    assert.strictEqual(remitAfterWithdrawal.secret, nullHash, "ledger secret not cleared after withdrawl");
+  });
+
+  beforeEach("deploy a fresh contract, generate secrets and deposit money", async () => {
+    remittance = await Remittance.new({ from: deployer });
+
+    const expectedHandlerKey = web3.utils.soliditySha3(
+      { type: "address", value: handler },
+      { type: "string", value: handlerPassword }
+    );
+    const expectedHashedSecret = web3.utils.soliditySha3(
+      { type: "bytes32", value: expectedHandlerKey },
       { type: "string", value: receiverPassword }
     );
 
-    const depositTxObj = await remittance.contract.methods.deposit(hashKey, handler).send({ from: sender, value: _sent });
+    const secrets = await remittance.contract.methods.generateSecret(handler, handlerPassword, receiverPassword).call();
+
+    assert.strictEqual(secrets.handlerKey, expectedHandlerKey, "did not generate expected handler key");
+    assert.strictEqual(secrets.hashedSecret, expectedHashedSecret, "did not generate expected hashed secret");
+
+    const depositTxObj = await remittance.contract.methods
+      .deposit(secrets.hashedSecret, secrets.handlerKey)
+      .send({ from: sender, value: _sent });
 
     assert.isDefined(depositTxObj, "deposit function did not get mined/execute");
+  });
 
+  it("should give owed money when both passwords are correct", async () => {
     const weiBeforeWithdraw = await web3.eth.getBalance(handler);
 
     const withdrawTxObj = await remittance.contract.methods
@@ -80,21 +141,56 @@ contract("Remittance", (accounts) => {
     expect(afterBalance).to.be.a.bignumber.that.equals(expectedAfterBalance);
   });
 
-  it("should revert if the withdrawer address is not owed", async () => {
-    const _sent = 20;
-    const handlerPassword = "123456";
-    const receiverPassword = "abcdef";
-    const hashKey = web3.utils.soliditySha3(
-      { type: "string", value: handlerPassword },
-      { type: "string", value: receiverPassword }
+  it("should revert when given empty password(s)", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw("", receiverPassword).send({ from: handler, value: 0 }),
+      "handlerPassword can not be empty"
     );
 
-    const depositTxObj = await remittance.contract.methods.deposit(hashKey, handler).send({ from: handler, value: _sent });
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw(handlerPassword, "").send({ from: handler, value: 0 }),
+      "receiverPassword can not be empty"
+    );
+  });
 
-    assert.isDefined(depositTxObj, "deposit function did not get mined/executed");
+  it("should revert when given identical passwords", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw(handlerPassword, handlerPassword).send({ from: handler, value: 0 }),
+      "passwords can not be the same"
+    );
+  });
 
+  it("should revert if the withdrawer address is not owed", async () => {
     await truffleAssert.reverts(
       remittance.contract.methods.withdraw(handlerPassword, receiverPassword).send({ from: randomAddress, value: 0 }),
+      "Sender is not owed a withdrawal"
+    );
+  });
+
+  it("should revert if passwords are incorrect", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw("wronghandlerPassword", "wrongReceiverPassword").send({ from: handler, value: 0 }),
+      "Sender is not owed a withdrawal"
+    );
+  });
+
+  it("should revert if receiver password is incorrect", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw(handlerPassword, "wrongReceiverPassword").send({ from: handler, value: 0 }),
+      "Passwords are incorrect"
+    );
+  });
+
+  it("should revert if handler password is incorrect", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw("wronghandlerPassword", receiverPassword).send({ from: handler, value: 0 }),
+      "Sender is not owed a withdrawal"
+    );
+  });
+
+  it("should revert if passwords are reversed", async () => {
+    await truffleAssert.reverts(
+      remittance.contract.methods.withdraw(receiverPassword, handlerPassword).send({ from: handler, value: 0 }),
       "Sender is not owed a withdrawal"
     );
   });
