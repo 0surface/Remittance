0057841115ab5d1aeed565704cfe353db97faa10
0surface <tms.0xvoid@gmail.com>
2020-11-20 10:15:06 +0000

    add test generate remit Key from web3 soliditySha3

--- test/remittance_generateKey_tests.js
+++ test/remittance_generateKey_tests.js
@@ -34,6 +34,20 @@ contract("Remittance", (accounts) => {
         });
     });
 
+    it("should be able to generate remit Key from web3 soliditySha3", async () => {
+      const web3SoliditySha3RemitKey = web3.utils.soliditySha3(
+        { type: "bytes32", value: receiverPassword },
+        { type: "address", value: remitter },
+        { type: "address", value: remittance.address }
+      );
+
+      const soliditykeccak256RemitKey = await remittance.contract.methods
+        .generateKey(remitter, receiverPassword)
+        .call({ from: sender });
+
+      assert.strictEqual(web3SoliditySha3RemitKey, soliditykeccak256RemitKey, "web3 and keccak256 generated keys don't match");
+    });
+
     it("should revert if given empty string as a password", async () => {
       await truffleAssert.reverts(
         remittance.contract.methods.generateKey(remitter, emptyString).call({ from: sender }),
